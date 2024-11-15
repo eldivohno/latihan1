bb1efc22162c6a51626ae13cdd8a5194377875f9
eldivohno <moheldivo@gmail.com>
2024-11-15 19:50:56 +0700

    UpdateOOP

--- calcOOP.py
+++ calcOOP.py
@@ -1,34 +1,25 @@
 class kalkulator:
-    def tambah(self, num1, num2):
-        return num1+num2
+    def tambah(self, a, b):
+        return a+b
     
-
-    def kurang(self, num1, num2):
-        sub = num1-num2
-        return sub
+    def kurang(self, a, b):
+        return a-b
     
-    def kali(self, num1, num2):
-        self.a = num1
-        self.b = num2
-        tim = self.a*self.b
-        return tim
+    def kali(self, a, b):
+        return a*b
 
-    def bagi(self, num1, num2):
-        self.a = num1
-        self.b = num2
-        if(self.b!=0):
-            div = self.a/self.b
-            return div
+    def bagi(self, a, b):
+        if(b!=0):return a/b
         else: return "tidak bisa membagi dengan 0"
     
 
 
 def main():
     calc = kalkulator()
-    print("Tambah :", calc.tambah(10, 5))
-    print("Kurang :", calc.kurang(10, 5))
-    print("Kali :", calc.kali(10, 5))
-    print("Bagi :", calc.bagi(10, 0))
+    print("Tambah :", calc.tambah(100, 27))
+    print("Kurang :", calc.kurang(130, 3))
+    print("Kali :", calc.kali(127, 1))
+    print("Bagi :", calc.bagi(635, 5))
 
 if __name__ == "__main__":
     main()
